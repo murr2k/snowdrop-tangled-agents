b3bc2ac8c407c8eeb8aa75ac7f5af42b7e7c11ff
Geordie Rose <greatblueheron@users.noreply.github.com>
2025-10-02 13:45:29 -0600

    expose agents

--- snowdrop_tangled_agents/__init__.py
+++ snowdrop_tangled_agents/__init__.py
@@ -1,7 +1,11 @@
+from snowdrop_tangled_agents.agents.mcts_agent import MCTSAgent
 from snowdrop_tangled_agents.agents.random_agent import RandomRandyAgent
 from snowdrop_tangled_agents.agents import random_agent
+from snowdrop_tangled_agents.agents.alphazero_agent import AlphaZeroAgent
 
 __all__ = [
     'RandomRandyAgent',
-    'random_agent'
+    'random_agent',
+    'MCTSAgent',
+    'AlphaZeroAgent'
 ]
